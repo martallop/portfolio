6c35bfa96f0d30c6e405e5baa9f94c19d68dfa1f
martallop <martagllop@gmail.com>
2017-03-20 10:59:04 -0400

    carousel and mouseover javascript working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,76 @@
-// $(document).ready(function(){
-//     $(".aQli").mouseover(function(){
+$(document).ready(function(){
 
-//     });
-//     $(".aQli").mouseout(function(){
+  // start of carousel
+  $('.carousel').slick({
+    dots: true,
+    infinite: true,
+    // speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    adaptiveHeight: true,
 
-//     });
-// });
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  // end of carousel
+
+  $('.project-homepage').mouseover(function() {
+    // $('.info-mouseover').show();
+    $(this).children('.info-mouseover').show();
+
+     // $('.info-mouseover').addClass('displayOverlay');
+  });
+
+  $('.project-homepage').mouseout(function() {
+      // $('.info-mouseover').removeClass('displayOverlay');
+      // $('.info-mouseover').attr('id', '');
+       $('.info-mouseover').hide();
+  });
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
